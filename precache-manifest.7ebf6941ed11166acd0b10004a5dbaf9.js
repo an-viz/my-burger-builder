a68a4581c6cdca71784f30834f9fe4b2f044cecb
Anirudh Viswanath <anirudhviswanath08@gmail.com>
2020-11-25 17:58:45 +0530

    Updates

---
+++ precache-manifest.7ebf6941ed11166acd0b10004a5dbaf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5401a2054683b05aedefd3ea200dca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845f0d7d16f2f83e581a",
+    "url": "/static/css/main.1821a248.chunk.css"
+  },
+  {
+    "revision": "e82f90632befa4ff1cae",
+    "url": "/static/js/2.89ef8856.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.89ef8856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845f0d7d16f2f83e581a",
+    "url": "/static/js/main.1d3cbff8.chunk.js"
+  },
+  {
+    "revision": "54fa86d2aea86a318381",
+    "url": "/static/js/runtime-main.5c3b6bd7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
