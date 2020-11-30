8201ed29a18d38c1fdbabe5df2746b5dbd453e94
Anirudh Viswanath <anirudhviswanath08@gmail.com>
2020-12-01 00:23:41 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename={ProcessingInstruction.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
